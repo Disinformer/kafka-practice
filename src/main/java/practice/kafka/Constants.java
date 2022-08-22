86c8d053d3b97870c9c8281bd9c285579de93427
dvigal <alektron@yandex.ru>
2022-08-22 23:56:00 +0300

    Примеры, демострирующие работу с Kafka API

---
+++ src/main/java/practice/kafka/Constants.java
@@ -0,0 +1,11 @@
+package practice.kafka;
+
+public class Constants {
+
+    public static final String CONSUMER_GROUP_NAME = "messages-group";
+
+    public static final String TOPIC_NAME = "messages";
+
+    public static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+
+}
